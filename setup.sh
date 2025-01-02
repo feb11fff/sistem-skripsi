53567bc903609d93399918f6386e2822b115ee94
feb11fff <136089445+feb11fff@users.noreply.github.com>
2025-01-02 21:18:39 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
+mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"200411100106@student.trunojoyo.ac.id\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
